5ee13c1bedbf41a8ee0ba5602344d86b2093c95e
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-03-26 13:16:41 -0500

    birthday js

---
+++ src/js/birthday.js
@@ -0,0 +1,18 @@
+const birthdayInput = document.querySelector(".birthday__input");
+const birthdayResult = document.querySelector(".birthday__text");
+const birthdayBtn = document.querySelector(".birthday__btn");
+// birthdayInput.addEventListener("input", (event) => {
+//   const birthdayYear = event.target.value;
+birthdayBtn.addEventListener("click", () => {
+  birthdayResult.style.opacity = 1;
+  if (
+    birthdayInput.value % 4 === 0 &&
+    (birthdayInput.value % 100 === !0 || birthdayInput.value % 400 === 0)
+  ) {
+    birthdayResult.textContent = "Ви народилися у високосний рік!";
+  } else {
+    birthdayResult.textContent = "Ви народилися не у високосний рік!";
+    birthdayResult.style.color = "#900";
+  }
+});
+// });
