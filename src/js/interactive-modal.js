9f7f0e931183922130005f804aaf1978bfbc4cea
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-04-06 13:32:01 -0500

    rebuild

--- src/js/interactive-modal.js
+++ src/js/interactive-modal.js
@@ -1,6 +1,10 @@
 const interactiveBtn = document.querySelector(".header__interactive-btn");
 const interactiveModal = document.querySelector(".interactive-modal");
 const interactiveBackdrop = document.querySelector(".backdrop");
+const birthday = document.querySelector(".birthday");
+const guessNumber = document.querySelector(".guess-number");
+const rockPaperScissors = document.querySelector("rock-paper-scissors");
+
 
 interactiveBtn.addEventListener("click", () => {
   interactiveModal.style.display = "block";
