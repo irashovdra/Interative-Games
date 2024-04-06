0f4a35a163aa9ecae6737b234a42b8e0dea20ad0
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-04-06 13:27:34 -0500

    added sass to json

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,15 +1,9 @@
-const interactiveBtn = document.querySelector(".interactive-list__btn");
 const headerModal = document.querySelector(".header-modal");
 const interactiveModal = document.querySelector(".interactive-modal");
 const headerModalInput = document.querySelector(".header-modal__input");
 const headerModalClose = document.querySelector(".header-modal__close");
 const headerSpan = document.querySelector(".header__user");
 const headerModalBtn = document.querySelector(".header-modal__btn");
-
-interactiveBtn.addEventListener("click", () => {
-  headerModal.style.display = "block";
-  interactiveModal.style.display = "none";
-});
 
 headerModalClose.addEventListener("click", () => {
   headerModal.style.display = "none";
