2a251d5a5254eb5b33f6d1302589ae48dc2e8323
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-04-30 20:16:23 -0500

    created a collision in dino.js

--- src/js/dinosaur.js
+++ src/js/dinosaur.js
@@ -1,5 +1,8 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+let isGameStarted = false;
+let isCactusMoving = false;
+let cactusInterval;
 
 function jump() {
   if (dino.classList != "jump") {
@@ -7,26 +10,51 @@ function jump() {
 
     setTimeout(function () {
       dino.classList.remove("jump");
-    }, 300);
+    }, 500);
   }
 }
 
-let isAlive = setInterval(function () {
-  // get current dino Y position
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-
-  // get current cactus X position
-  let cactusLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left")
-  );
-
-  // detect collision
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    // collision
-    alert("Game Over!");
+function startGame() {
+  if (!isGameStarted) {
+    isGameStarted = true;
+    isCactusMoving = true;
+    cactusInterval = setInterval(moveCactus, 10);
   }
-}, 10);
+}
 
-document.addEventListener("keydown", function (event) {
+function moveCactus() {
+  if (isCactusMoving) {
+    let cactusLeft = parseInt(
+      window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    cactus.style.left = cactusLeft - 1 + "px"; // Змінено швидкість руху на 1px на кожному кадрі
+    let dinoTop = parseInt(
+      window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+      clearInterval(cactusInterval); // Зупиняємо рух кактуса
+      isCactusMoving = false;
+      alert("Game Over!");
+      isGameStarted = false; // Позначаємо гру як завершену
+    }
+  }
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowUp") {
+    startGame();
+  }
   jump();
+});
+
+// Додали обробник подій для кнопки "ОК" у вікні alert
+window.addEventListener("load", function () {
+  let alertBox = document.querySelector(".alert");
+  if (alertBox) {
+    alertBox.addEventListener("click", function () {
+      if (!isGameStarted) {
+        location.reload();
+      }
+    });
+  }
 });
--- src/js/dinosaur1.js
+++ src/js/dinosaur1.js
@@ -1,75 +1,31 @@
-const dino = document.querySelector(".dinosaur__img");
-const cactus = document.querySelector(".dinosaur__cactus-img");
-let isGameStarted = true;
-let isGameEnded = false;
-let dinoCounter = 0;
-let cactusCounter = 0;
-cactus.style.transform = `translateX(${cactusCounter}px)`;
-let dinoRect = dino.getBoundingClientRect();
-let cactusRect = cactus.getBoundingClientRect();
-// console.log(dinoRect);
-// console.log(cactusRect);
-function gameStart() {
-  isGameStarted = true;
-  // startCactusMovement();
-  // cactusMovement();
-}
+// const dino = document.getElementById("dino");
+// const cactus = document.getElementById("cactus");
 
-function dinoJump() {
-  dinoCounter = -90;
-  dino.style.transform = `translateY(${dinoCounter}px)`;
-}
+// function jump() {
+//   if (dino.classList != "jump") {
+//     dino.classList.add("jump");
 
-function dinoDown() {
-  dinoCounter = 0;
-  dino.style.transform = `translateY(${dinoCounter}px)`;
-}
+//     setTimeout(function () {
+//       dino.classList.remove("jump");
+//     }, 500);
+//   }
+// }
 
-document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
-    gameStart();
-  }
-});
+// let isAlive = setInterval(function () {
+//   // get current dino Y position
+//   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
 
-document.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowUp") {
-    dinoJump();
-  }
-});
+//   // get current cactus X position
+//   let cactusLeft = parseInt(
+//     window.getComputedStyle(cactus).getPropertyValue("left")
+//   );
 
-document.addEventListener("keyup", () => {
-  setTimeout(dinoDown, 500);
-});
+//   // detect collision
+//     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+//       alert("Game Over!");
+//     }
+// }, 10);
 
-//  РУХ КАКТУСА
-
-function cactusMovement() {
-  setInterval(() => {
-    cactusCounter++;
-    cactus.style.right = `${cactusCounter}px`;
-  }, 1);
-  if (detectCollision(dino, cactus)) {
-    isGameEnded = true;
-    alert("game over");
-  }
-  // Коли - 1000 запустити знову
-  // Коли динозаврик внизу і кактус на його місці - game over, відслідкувати через right i dinoCounter
-  console.log(cactusCounter);
-}
-cactusMovement();
-let cactusMovementId;
-
-function endGame() {
-  isGameEnded = true;
-  alert("Game Over!");
-  clearInterval(cactusMovementId);
-}
-
-function detectCollision(a, b) {
-  return (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.y + a.height > b.y
-  );
-}
+// document.addEventListener("keydown", function (event) {
+//   jump();
+// });
